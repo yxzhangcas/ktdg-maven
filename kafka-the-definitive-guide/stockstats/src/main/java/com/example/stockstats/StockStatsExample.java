e6ac43ff3a34fd56e3c220f80e215d2fbaf7824f
zhangyxd <zhangyxd@sugon.com>
2020-03-13 15:09:15 +0800

    add stockstats with version 2.0.0

---
+++ kafka-the-definitive-guide/stockstats/src/main/java/com/example/stockstats/StockStatsExample.java
@@ -0,0 +1,107 @@
+package com.example.stockstats;
+
+import com.example.stockstats.serde.JsonDeserializer;
+import com.example.stockstats.serde.JsonSerializer;
+import com.example.stockstats.serde.WrapperSerde;
+import com.example.stockstats.model.Trade;
+import com.example.stockstats.model.TradeStats;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.kstream.WindowedSerdes;
+import org.apache.kafka.streams.state.WindowStore;
+
+import java.util.Properties;
+
+/**
+ * Input is a stream of trades
+ * Output is two streams: One with minimum and avg "ASK" price for every 10 seconds window
+ * Another with the top-3 stocks with lowest minimum ask every minute
+ */
+public class StockStatsExample {
+
+    public static void main(String[] args) throws Exception {
+
+        Properties props;
+        /*
+        if (args.length==1)
+            props = LoadConfigs.loadConfig(args[0]);
+        else
+            props = LoadConfigs.loadConfig();
+         */
+        props = LoadConfigs.loadConfig("stockstats.properties");
+
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stockstat-2");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, TradeSerde.class.getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // creating an AdminClient and checking the number of brokers in the cluster, so I'll know how many replicas we want...
+
+        AdminClient ac = AdminClient.create(props);
+        DescribeClusterResult dcr = ac.describeCluster();
+        int clusterSize = dcr.nodes().get().size();
+
+        if (clusterSize<3)
+            props.put("replication.factor",clusterSize);
+        else
+            props.put("replication.factor",3);
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, Trade> source = builder.stream(Constants.STOCK_TOPIC);
+
+        KStream<Windowed<String>, TradeStats> stats = source
+                .groupByKey()
+                .windowedBy(TimeWindows.of(5000).advanceBy(1000))
+                .<TradeStats>aggregate(() -> new TradeStats(),(k, v, tradestats) -> tradestats.add(v),
+                        Materialized.<String, TradeStats, WindowStore<Bytes, byte[]>>as("trade-aggregates")
+                                .withValueSerde(new TradeStatsSerde()))
+                .toStream()
+                .mapValues((trade) -> trade.computeAvgPrice());
+
+        stats.to("stockstats-output", Produced.keySerde(WindowedSerdes.timeWindowedSerdeFrom(String.class)));
+
+        Topology topology = builder.build();
+
+        KafkaStreams streams = new KafkaStreams(topology, props);
+
+        System.out.println(topology.describe());
+
+        streams.cleanUp();
+
+        streams.start();
+
+        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+
+    static public final class TradeSerde extends WrapperSerde<Trade> {
+        public TradeSerde() {
+            super(new JsonSerializer<Trade>(), new JsonDeserializer<Trade>(Trade.class));
+        }
+    }
+
+    static public final class TradeStatsSerde extends WrapperSerde<TradeStats> {
+        public TradeStatsSerde() {
+            super(new JsonSerializer<TradeStats>(), new JsonDeserializer<TradeStats>(TradeStats.class));
+        }
+    }
+
+}
---
+++ kafka-the-definitive-guide/stockstats/src/main/java/com/example/stockstats/model/Trade.java
@@ -0,0 +1,33 @@
+package com.example.stockstats.model;
+
+/**
+ * Created by gwen on 1/22/17.
+ */
+public class Trade {
+
+    String type;
+    String ticker;
+    double price;
+    int size;
+
+    public Trade(String type, String ticker, double price, int size) {
+        this.type = type;
+        this.ticker = ticker;
+        this.price = price;
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "type='" + type + '\'' +
+                ", ticker='" + ticker + '\'' +
+                ", price=" + price +
+                ", size=" + size +
+                '}';
+    }
+}
